68887aab8a983ed2454def854487984cff5ecb8d
Cameron Houck <cameronhouck1@gmail.com>
2021-06-30 08:04:16 -0400

    adding intern and manager tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("Can set school name via constructor", () => {
+  const testValue = "schoolsName";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test('getRole() should return "Intern"', () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "school");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school name via getSchool()", () => {
+  const testValue = "schoolsName";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
