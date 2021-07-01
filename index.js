0a64e4dd6f30d93e2fd62607ea14c0192a3a21d3
Cameron Houck <cameronhouck1@gmail.com>
2021-07-01 01:03:28 -0400

    adding userprompt function for about the employee

--- index.js
+++ index.js
@@ -33,22 +33,93 @@ const internQuestion = [
   },
 ];
 const additionalMember = () => {
-    inquirer.prompt([
+  inquirer
+    .prompt([
       {
-        type:'confirm',
-        name: 'addMore',
-        message: 'Would you like to add another employee?',
+        type: "confirm",
+        name: "addMore",
+        message: "Would you like to add another employee?",
       },
     ])
     .then((userInput) => {
       if (userInput.addMore === true) {
         console.log("prompting for another employee");
-        questions();
-    }
-    else {
-      const htmlCard = generateHTMLCard(teamMembers);
-      const htmlPage = generateHTMLPage(htmlCard);
-      writeFileAsync('./dist/index.html', htmlPage);
-  }
+        userPrompt();
+      } else {
+        const htmlCard = generateHTMLCard(teamMembers);
+        const htmlPage = generateHTMLPage(htmlCard);
+        writeFileAsync("./dist/index.html", htmlPage);
+      }
     });
-  };
+};
+
+const userPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Select a role card you'd like to build.",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is your employee's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your employees email adress.",
+      },
+    ])
+    .then((userInput) => {
+      if (userInput.role === "Manager") {
+        inquirer.prompt(managerQuestion).then((userInput2) => {
+          const ManagerDude = new Manager(
+            userInput.name,
+            userInput.id,
+            userInput.email,
+            userInput2.officeNumber
+          );
+          teamMembers.push(ManagerDude);
+          promptAnotherEmployee();
+        });
+      } else if (userInput.role === "Engineer") {
+        inquirer.prompt(engineerQuestion).then((userInput2) => {
+          const EngineerDude = new Engineer(
+            userInput.name,
+            userInput.id,
+            userInput.email,
+            userInput2.github
+          );
+          teamMembers.push(EngineerDude);
+          promptAnotherEmployee();
+        });
+      } else if (userInput.role === "Intern") {
+        inquirer.prompt(internQuestion).then((userInput2) => {
+          const InternDude = new Intern(
+            userInput.name,
+            userInput.id,
+            userInput.email,
+            userInput2.schoolName
+          );
+          teamMembers.push(InternDude);
+          promptAnotherEmployee();
+        });
+      } else {
+        const employee = new Employee(
+          userInput.name,
+          userInput.id,
+          userInput.email,
+          userInput.role
+        );
+        teamMembers.push(employee);
+      }
+    });
+};
